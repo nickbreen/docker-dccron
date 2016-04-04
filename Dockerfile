04c875ce0cc17ff55899c4a7bda0e832d07c37ae
Nick Breen <nick@foobar.net.nz>
2016-04-04 23:42:58 +1200

    working

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,10 @@ FROM nickbreen/cron:v1.0.0
 
 MAINTAINER Nick Breen <nick@foobarnet.nz>
 
-RUN apt-get update && DEBIAN_FRONEND=noninteractive apt-get install -qqy python-pip && apt-get clean
+RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -qqy python-pip jq && apt-get clean
 
 RUN pip install python-dockercloud
 
-COPY run /etc/service/dccron/run
-
-COPY dccron.py /opt/dccron/
-
 ENV DOCKERCLOUD_USER="" DOCKERCLOUD_APIKEY=""
+
+COPY dccron.py /etc/service/dccron/run
---
+++ dccron.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from sys import stdin
+from dockercloud import Events
+from subprocess import Popen, PIPE, STDOUT
+from json import dumps, loads
+
+KEYS = ('type','action','state','user','command')
+
+def process_event(event):
+    e = loads(event)
+    for js in job_specs:
+        if (("*" == js['type'] or js['type'] == e['type']) and
+            ("*" == js['action'] or js['action'] == e['action']) and
+            ("*" == js['state'] or js['state'] == e['state'])):
+            command = ["su", '-', js['user'], "-c", js['command'], "-", e['type'], e['action'], e['resource_uri'], e['state'], e['uuid'], e['datetime']] + e['parents']
+            p = Popen(command, stdin=PIPE)
+            p.communicate(input=dumps(event))[0]
+
+job_specs = [dict(zip(KEYS, line.strip().split(None, len(KEYS)-1))) for line in stdin if not line.lstrip().startswith('#') ]
+
+events = Events()
+events.on_message(process_event)
+events.run_forever()
